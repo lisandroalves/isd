1aa0bb1f5abefe4849e9b6e895321d5fce22f3fa
lisandroalves <trustals@gmail.com>
2014-12-08 22:28:43 +0000

    Delete funcs.h

--- funcs.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCS_H
-#define FUNCS_H
-
-float calcula_t (float t_ant, float step);
-float calcula_s (float s_ant, float i_ant, float step);
-float calcula_i (float s_ant, float i_ant, float step);
-
-
-#endif
